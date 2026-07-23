cac9b0924ff4dd51f81d5b107aeac04227e908df
Tarnished <susan.dev291@gmail.com>
2026-07-23 17:57:14 +0545

    Listed todo

---
+++ src-tauri/src/scanner.rs
@@ -0,0 +1,5 @@
+// TODO:: Write scanner that scan dir which will be selected by user and its should filter out mp3 file and do recursive and extratc metadata and return to it 
+
+
+
+// Metadata should contain title, artist, album, duration -> This should be use to fetch respective lyrics of the song
